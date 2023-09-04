13dcc9402e2ffd28a7a0a1b074df05316079e5e5
Volkan YILMAZ <volkan.yilmazboun@gmail.com>
2023-09-04 22:39:02 +0200

    Delete data directory

--- data/remote/dto/TeamMember.kt
+++
@@ -1,7 +0,0 @@
-package com.yilmazvolkan.crytocurrencyapp.data.remote.dto
-
-data class TeamMember(
-    val id: String,
-    val name: String,
-    val position: String
-)
--- data/remote/dto/Whitepaper.kt
+++
@@ -1,6 +0,0 @@
-package com.yilmazvolkan.crytocurrencyapp.data.remote.dto
-
-data class Whitepaper(
-    val link: String,
-    val thumbnail: String
-)
