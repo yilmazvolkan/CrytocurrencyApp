5141e12ab39cc9150a8d3da58da48cc26d6dabac
Volkan YILMAZ <volkan.yilmazboun@gmail.com>
2023-09-04 22:45:52 +0200

    Add entry point to app

---
+++ app/src/main/java/com/yilmazvolkan/crytocurrencyapp/presentation/ui/theme/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.yilmazvolkan.crytocurrencyapp.presentation.ui.theme
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.yilmazvolkan.crytocurrencyapp.presentation.Screen
+import com.yilmazvolkan.crytocurrencyapp.presentation.coin_details.CoinDetailScreen
+import com.yilmazvolkan.crytocurrencyapp.presentation.coin_list.CoinListScreen
+import com.yilmazvolkan.crytocurrencyapp.ui.theme.CrytocurrencyAppTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            CrytocurrencyAppTheme {
+                Surface(color = MaterialTheme.colorScheme.background) {
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = Screen.CoinListScreen.route
+                    ) {
+                        composable(
+                            route = Screen.CoinListScreen.route
+                        ) {
+                            CoinListScreen(navController)
+                        }
+                        composable(
+                            route = Screen.CoinDetailScreen.route + "/{coinId}"
+                        ) {
+                            CoinDetailScreen()
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
